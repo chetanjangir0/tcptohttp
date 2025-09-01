2ed6e47ff4ef505417d8dcc22fdbf3cc71c95188
chetanjangir0 <chetanrjj2002@gmail.com>
2025-09-01 14:32:03 +0530

    read lines from a file but use goroutines and channels to transfer the lines

--- main.go
+++ main.go
@@ -3,37 +3,55 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
 	file, err := os.Open("./message.txt")
 	if err != nil {
-		panic(err)
+		log.Fatal("error", err)
 	}
 
-	str := ""
-	for {
-		b := make([]byte, 8)
-		n, err := file.Read(b)
-		if err != nil {
-			break
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		defer file.Close()
+
+		str := ""
+		for {
+			b := make([]byte, 8)
+			n, err := file.Read(b)
+			if err != nil {
+				break
+			}
+
+			b = b[:n]
+
+			if i := bytes.IndexByte(b, '\n'); i != -1 {
+				str += string(b[:i])
+				b = b[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(b)
+
 		}
 
-		b = b[:n]
-
-		if i := bytes.IndexByte(b, '\n'); i != -1 {
-			str += string(b[:i])
-			b = b[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
+		if len(str) != 0 {
+			out <- str
 		}
-		str += string(b)
+	}()
 
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
-
+	return out
 }
