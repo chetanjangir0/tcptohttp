24dcad92b82c0099d1710b7209ab49c8a591d1fa
chetanjangir0 <chetanrjj2002@gmail.com>
2025-09-02 15:19:18 +0530

    added test for request line (start line) parser for tdd

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,14 @@
 package request
+
+type RequestLine struct{
+	HttpVersion string
+	RequestTarget string
+	Method string
+}
+
+type Request struct{
+	RequestLine RequestLine
+}
+func RequestFromReader(reader io.Reader) (*Request, error){
+
+}
